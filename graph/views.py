7e98fbd57d3edd100fda38a3d0f6002c99214792
Temigo <temigo@gmx.com>
2015-12-22 19:34:15 +0100

    Infections working

--- graph/views.py
+++ graph/views.py
@@ -9,36 +9,43 @@ import json
 ######################################################################
 # Index
 def index(request):
-    current_index = 0
-    current_index_infection = 0
-    if request.method == 'POST':
-        current_index = int(request.POST.get('graph_id', 1)-1)
-        current_index_infection = int(request.POST.get('infection_id', 1))-1
-        request.session.__setitem__('current_index', current_index)
-        request.session.__setitem__('current_index_infection', current_index_infection)
-    else:
-        if request.session.__contains__('current_index'):
-        	current_index = request.session.__getitem__('current_index')
-        if request.session.__contains__('current_index_infection'):
-            current_index_infection = request.session.__getitem__('current_index_infection')
-    
+    #request.session.flush()
+    current_index = request.session.get('current_index', 0)
+    current_index_infection = request.session.get('current_index_infection', 0)
 
-    latest_graph_list = Graph.objects.order_by('name')
+    if request.method == 'POST':
+        if request.POST.get('request') == 'graph':
+            current_index = int(request.POST.get('graph_id'))-1
+            request.session.__setitem__('current_index', current_index)
+        if request.POST.get('request') == 'infection':
+            current_index_infection = int(request.POST.get('infection_id'))-1        
+            request.session.__setitem__('current_index_infection', current_index_infection)
+    
+    
+    latest_graph_list = Graph.objects.order_by('id')
     #latest_infection_list = Infection.objects.order_by('name')
-    latest_infection_list = Infection.objects.filter(graph=latest_graph_list[current_index]).order_by('name')
+    latest_infection_list = Infection.objects.filter(graph=latest_graph_list[current_index]).order_by('id')
     #current_graph_data = json.dumps(latest_graph_list[0].data)
-    
-    current_graph_data = latest_graph_list[current_index].data
-    current_infection_graph_data = latest_infection_list[current_index_infection].data
-    
+
     context = {'latest_graph_list': latest_graph_list, 
     'latest_infection_list': latest_infection_list, 
-    'nodes': json.dumps(current_graph_data["nodes"]), 
-    'infected_nodes': json.dumps(current_infection_graph_data["nodes"]),
-    'links': json.dumps(current_graph_data["links"]), 
+    'nodes': None, 
+    'links': None, 
+    'infected_nodes': [],
     'current_index': current_index+1, 
     'current_index_infection': current_index_infection+1}
     
+    current_graph_data = None
+    current_infection_graph_data = None
+    if latest_graph_list:
+        current_graph_data = latest_graph_list[current_index].data
+        context['nodes'] = json.dumps(current_graph_data["nodes"])
+        context['links'] = json.dumps(current_graph_data["links"])
+    if latest_infection_list:
+        current_infection_graph_data = latest_infection_list[current_index_infection].data
+        context['infected_nodes'] = json.dumps(current_infection_graph_data["nodes"])
+
+    print(context)
     return render(request, 'graph/index.html', context)
 
 ######################################################################
